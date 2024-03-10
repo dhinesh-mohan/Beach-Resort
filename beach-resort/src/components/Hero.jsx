e0efc6b3a3beefe91541d9d98c81fe96c1a194b4
Dhinesh M <dhineshambi03@gmail.com>
2024-03-10 20:16:13 +0530

    Banner

--- beach-resort/src/components/Hero.jsx
+++ beach-resort/src/components/Hero.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Hero = () => {
-  return <div className="hero-container"></div>;
+  return (
+    <div className="hero-container">
+      <div className="banner">
+        <h1>Luxurious Rooms</h1>
+        <div></div>
+        <p>Deluxe Rooms Starting At $299</p>
+        <button className="btn-primary">OUR ROOMS</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
